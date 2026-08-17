ebd0ba56a5a2711d6a9879b71025135e2b4e1af4
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-17 14:18:39 +0100

    R2-WP2.6A: remove temporary browser diagnostic hook

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,4 @@ import Phaser from 'phaser';
 import './style.css';
 import { gameConfig } from './game/config/gameConfig';
 
-const game = new Phaser.Game(gameConfig);
-
-if (new URLSearchParams(globalThis.location.search).has('scene')) {
-  const diagnosticGlobal = globalThis as typeof globalThis & {
-    __unicornValleyGame?: Phaser.Game;
-  };
-  diagnosticGlobal.__unicornValleyGame = game;
-}
+new Phaser.Game(gameConfig);
