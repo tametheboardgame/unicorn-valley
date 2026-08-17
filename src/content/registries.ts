d2263bf58eb0d35be3cf7257c1268c420c39a8fd
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-17 12:21:24 +0100

    Register Willow story dialogues

--- src/content/registries.ts
+++ src/content/registries.ts
@@ -8,6 +8,7 @@ import {
   QUESTS,
 } from './placeholderContent';
 import { R2_CHARACTERS } from './r2Characters';
+import { R2_DIALOGUES } from './r2Dialogues';
 import { R2_ITEMS } from './r2Items';
 import { R2_QUESTS } from './r2Quests';
 import type {
@@ -27,12 +28,14 @@ import { assertValidContent } from './validateContent';
 const ALL_ITEMS = [...ITEMS, ...R2_ITEMS] satisfies readonly ItemDefinition[];
 const ALL_CHARACTERS = [...CHARACTERS, ...R2_CHARACTERS] satisfies readonly CharacterDefinition[];
 const ALL_QUESTS = [...QUESTS, ...R2_QUESTS] satisfies readonly QuestDefinition[];
+const ALL_DIALOGUES = [...DIALOGUES, ...R2_DIALOGUES] satisfies readonly DialogueDefinition[];
 
 assertValidContent({
   ...PLACEHOLDER_CONTENT,
   items: ALL_ITEMS,
   characters: ALL_CHARACTERS,
   quests: ALL_QUESTS,
+  dialogues: ALL_DIALOGUES,
 });
 
 export const itemRegistry = new ContentRegistry<ItemId, ItemDefinition>('item', ALL_ITEMS);
@@ -47,5 +50,5 @@ export const discoveryRegistry = new ContentRegistry<DiscoveryId, DiscoveryDefin
 );
 export const dialogueRegistry = new ContentRegistry<DialogueId, DialogueDefinition>(
   'dialogue',
-  DIALOGUES,
+  ALL_DIALOGUES,
 );
