a390e2e05af9c50892fd000a53800b360e30b195
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-17 14:09:03 +0100

    R2-WP2.6A: expose game only in diagnostic mode

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,11 @@ import Phaser from 'phaser';
 import './style.css';
 import { gameConfig } from './game/config/gameConfig';
 
-new Phaser.Game(gameConfig);
+const game = new Phaser.Game(gameConfig);
+
+if (new URLSearchParams(globalThis.location.search).has('scene')) {
+  const diagnosticGlobal = globalThis as typeof globalThis & {
+    __unicornValleyGame?: Phaser.Game;
+  };
+  diagnosticGlobal.__unicornValleyGame = game;
+}
