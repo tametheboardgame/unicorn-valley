9074dbe6b7fcb19b38ea0b0e9139a74ed6896251
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-17 12:18:13 +0100

    Include Willow in character registry

--- src/content/registries.ts
+++ src/content/registries.ts
@@ -7,6 +7,7 @@ import {
   PLACEHOLDER_CONTENT,
   QUESTS,
 } from './placeholderContent';
+import { R2_CHARACTERS } from './r2Characters';
 import { R2_ITEMS } from './r2Items';
 import { R2_QUESTS } from './r2Quests';
 import type {
@@ -24,18 +25,20 @@ import type {
 import { assertValidContent } from './validateContent';
 
 const ALL_ITEMS = [...ITEMS, ...R2_ITEMS] satisfies readonly ItemDefinition[];
+const ALL_CHARACTERS = [...CHARACTERS, ...R2_CHARACTERS] satisfies readonly CharacterDefinition[];
 const ALL_QUESTS = [...QUESTS, ...R2_QUESTS] satisfies readonly QuestDefinition[];
 
 assertValidContent({
   ...PLACEHOLDER_CONTENT,
   items: ALL_ITEMS,
+  characters: ALL_CHARACTERS,
   quests: ALL_QUESTS,
 });
 
 export const itemRegistry = new ContentRegistry<ItemId, ItemDefinition>('item', ALL_ITEMS);
 export const characterRegistry = new ContentRegistry<CharacterId, CharacterDefinition>(
   'character',
-  CHARACTERS,
+  ALL_CHARACTERS,
 );
 export const questRegistry = new ContentRegistry<QuestId, QuestDefinition>('quest', ALL_QUESTS);
 export const discoveryRegistry = new ContentRegistry<DiscoveryId, DiscoveryDefinition>(
