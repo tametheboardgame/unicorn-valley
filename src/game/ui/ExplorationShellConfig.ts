c742b50c1b95abc00f70410a0a791c941065213f
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-24 12:38:26 +0100

    R5-WP5.9B separate pure shell configuration

---
+++ src/game/ui/ExplorationShellConfig.ts
@@ -0,0 +1,19 @@
+export const EXPLORATION_SHELL_SCENES = new Set([
+  'MoonflowerGladeScene',
+  'CottageInteriorScene',
+  'MoonflowerPatchScene',
+  'SunbeamVillageScene',
+  'RainbowMeadowScene',
+  'CrystalBrookScene',
+  'WhisperingWoodsScene',
+]);
+
+const DEDICATED_AUDIO_SCENES = new Set(['CrystalBrookScene', 'WhisperingWoodsScene']);
+
+export function supportsExplorationShell(sceneKey: string): boolean {
+  return EXPLORATION_SHELL_SCENES.has(sceneKey);
+}
+
+export function shellManagesSceneAudio(sceneKey: string): boolean {
+  return !DEDICATED_AUDIO_SCENES.has(sceneKey);
+}
