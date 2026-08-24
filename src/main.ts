ad893908c2b51966d1e5e8060ac04db05f52d48d
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-24 12:32:05 +0100

    R5-WP5.9B initialise shared exploration shell manager

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './style.css';
 import { gameConfig } from './game/config/gameConfig';
 import { getClickToMoveManager } from './game/input/ClickToMoveManager';
 import { installBrowserDiagnostics } from './game/testing/BrowserDiagnostics';
+import { getExplorationShellWorldManager } from './game/ui/ExplorationShellWorldManager';
 
 const game = new Phaser.Game(gameConfig);
 getClickToMoveManager(game);
+getExplorationShellWorldManager(game);
 installBrowserDiagnostics(game);
