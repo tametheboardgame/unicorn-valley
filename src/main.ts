f053b611a94f41b71e87c5435044b71610706ca1
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-28 11:15:09 +0100

    R6-WP6.18A initialise portrait creator controls

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Phaser from 'phaser';
 import './style.css';
 import './titlePortraitControls.css';
+import './creatorPortraitControls.css';
 import { gameConfig } from './game/config/gameConfig';
 import { getClickToMoveManager } from './game/input/ClickToMoveManager';
+import { getCreatorPortraitControlsManager } from './game/ui/CreatorPortraitControlsManager';
 import { getExplorationShellWorldManager } from './game/ui/ExplorationShellWorldManager';
 import { getTitlePortraitControlsManager } from './game/ui/TitlePortraitControlsManager';
 import { getR5FinalTighteningManager } from './game/visual/R5FinalTighteningManager';
@@ -12,6 +14,7 @@ const game = new Phaser.Game(gameConfig);
 getClickToMoveManager(game);
 getExplorationShellWorldManager(game);
 getTitlePortraitControlsManager(game);
+getCreatorPortraitControlsManager(game);
 getExplorationGeometryPresentationManager(game);
 getR5FinalTighteningManager(game);
 
