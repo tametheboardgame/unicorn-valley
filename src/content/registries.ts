3d458c9617e5bfc0bd73b8831daf62c3dd63412c
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-02 15:05:16 +0100

    Fix complete Village content registry integration

--- src/content/registries.ts
+++ src/content/registries.ts
@@ -73,5 +73,89 @@ const ALL_ITEMS = [
   ...R2_ITEMS,
   ...R3_ITEMS,
   ...R4_SHOP_ITEMS,
-  ...R4_PEBLE_ITEMS_PLACEHOLDER,
-] as const;
+  ...R4_PEBBLE_ITEMS,
+  ...R5_CRYSTAL_BROOK_ITEMS,
+  ...R5_CRYSTAL_BROOK_STORY_ITEMS,
+  ...R5_RACE_ITEMS,
+  ...R6_VILLAGE_ITEMS,
+] satisfies readonly ItemDefinition[];
+const ALL_CHARACTERS = [
+  ...CHARACTERS,
+  ...R2_CHARACTERS,
+  ...R3_CHARACTERS,
+  ...R4_EGG_CHARACTERS,
+  ...R4_PICNIC_CHARACTERS,
+  ...R4_PEBBLE_CHARACTERS,
+  ...R5_CRYSTAL_BROOK_STORY_CHARACTERS,
+  ...R5_LUMI_CHARACTERS,
+  ...R6_VILLAGE_CHARACTERS,
+] satisfies readonly CharacterDefinition[];
+const ALL_QUESTS = [
+  ...QUESTS,
+  ...R2_QUESTS,
+  ...R3_QUESTS,
+  ...R4_EGG_QUESTS,
+  ...R4_PICNIC_QUESTS,
+  ...R4_PEBBLE_QUESTS,
+  ...R5_CRYSTAL_BROOK_STORY_QUESTS,
+  ...R6_VILLAGE_QUESTS,
+] satisfies readonly QuestDefinition[];
+const ALL_DISCOVERIES = [
+  ...DISCOVERIES,
+  ...R3_DISCOVERIES,
+  ...R4_EGG_DISCOVERIES,
+  ...R4_SECRET_DISCOVERIES,
+  ...R4_PEBBLE_DISCOVERIES,
+  ...R5_CRYSTAL_BROOK_DISCOVERIES,
+  ...R5_CRYSTAL_BROOK_STORY_DISCOVERIES,
+  ...R5_WHISPERING_WOODS_DISCOVERIES,
+  ...R5_LUMI_DISCOVERIES,
+  ...R5_WEATHER_DISCOVERIES,
+  ...R5_RACE_DISCOVERIES,
+  ...R5_FIREFLY_LANTERN_DISCOVERIES,
+  ...R6_VILLAGE_DISCOVERIES,
+] satisfies readonly DiscoveryDefinition[];
+const ALL_DIALOGUES = [
+  ...DIALOGUES,
+  ...R2_DIALOGUES,
+  ...R3_DIALOGUES,
+  ...R4_DIALOGUES,
+  ...R4_FRIEND_VISIT_DIALOGUES,
+  ...R4_PICNIC_DIALOGUES,
+  ...R4_PEBBLE_DIALOGUES,
+  ...R5_CRYSTAL_BROOK_STORY_DIALOGUES,
+  ...R5_LUMI_DIALOGUES,
+] satisfies readonly DialogueDefinition[];
+const ALL_DIALOGUE_VARIANT_SETS = [
+  ...R4_DIALOGUE_VARIANT_SETS,
+  ...R4_PICNIC_DIALOGUE_VARIANT_SETS,
+] satisfies readonly DialogueVariantSet[];
+
+assertValidContent({
+  ...PLACEHOLDER_CONTENT,
+  items: ALL_ITEMS,
+  characters: ALL_CHARACTERS,
+  quests: ALL_QUESTS,
+  discoveries: ALL_DISCOVERIES,
+  dialogues: ALL_DIALOGUES,
+  dialogueVariantSets: ALL_DIALOGUE_VARIANT_SETS,
+});
+
+export const itemRegistry = new ContentRegistry<ItemId, ItemDefinition>('item', ALL_ITEMS);
+export const characterRegistry = new ContentRegistry<CharacterId, CharacterDefinition>(
+  'character',
+  ALL_CHARACTERS,
+);
+export const questRegistry = new ContentRegistry<QuestId, QuestDefinition>('quest', ALL_QUESTS);
+export const discoveryRegistry = new ContentRegistry<DiscoveryId, DiscoveryDefinition>(
+  'discovery',
+  ALL_DISCOVERIES,
+);
+export const dialogueRegistry = new ContentRegistry<DialogueId, DialogueDefinition>(
+  'dialogue',
+  ALL_DIALOGUES,
+);
+export const dialogueVariantSetRegistry = new ContentRegistry<
+  DialogueVariantSetId,
+  DialogueVariantSet
+>('dialogue variant set', ALL_DIALOGUE_VARIANT_SETS);
