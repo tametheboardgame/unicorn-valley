d347fc5a21d523792b2e71c016db1bedad99ac02
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-02 16:21:08 +0100

    Wire village interior diagnostic contracts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css';
 import './titlePortraitControls.css';
 import { gameConfig } from './game/config/gameConfig';
 import { getClickToMoveManager } from './game/input/ClickToMoveManager';
+import { getVillageInteriorContractManager } from './game/scenes/VillageInteriorContractManager';
 import { getExplorationShellWorldManager } from './game/ui/ExplorationShellWorldManager';
 import { browserHasRaceTouchCapability } from './game/ui/RaceTouchCapability';
 import { getTitlePortraitControlsManager } from './game/ui/TitlePortraitControlsManager';
@@ -15,6 +16,7 @@ getExplorationShellWorldManager(game);
 getTitlePortraitControlsManager(game);
 getExplorationGeometryPresentationManager(game);
 getR5FinalTighteningManager(game);
+getVillageInteriorContractManager(game);
 
 void import('./game/economy/EconomyRewardWorldManager').then(({ getEconomyRewardWorldManager }) => {
   getEconomyRewardWorldManager();
