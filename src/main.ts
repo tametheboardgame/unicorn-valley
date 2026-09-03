3c1d684e5b0e10501d947988882dca1dc2842fe6
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-03 16:51:28 +0100

    Wire exploration path polish into startup

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { browserHasRaceTouchCapability } from './game/ui/RaceTouchCapability';
 import { getTitlePortraitControlsManager } from './game/ui/TitlePortraitControlsManager';
 import { getR5FinalTighteningManager } from './game/visual/R5FinalTighteningManager';
 import { getExplorationGeometryPresentationManager } from './game/world/ExplorationGeometryPresentationManager';
+import { getExplorationPathPolishManager } from './game/world/ExplorationPathPolishManager';
 
 const game = new Phaser.Game(gameConfig);
 getClickToMoveManager(game);
@@ -17,6 +18,7 @@ getContinueRestoreManager(game);
 getExplorationShellWorldManager(game);
 getTitlePortraitControlsManager(game);
 getExplorationGeometryPresentationManager(game);
+getExplorationPathPolishManager(game);
 getR5FinalTighteningManager(game);
 getVillageInteriorContractManager(game);
 
