1ef0e13a97569406d65366cf9b4170e9cda2240c
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-15 14:44:18 +0100

    Use canonical final Glade presentation owner

--- src/game/visual/R5FinalTighteningManager.ts
+++ src/game/visual/R5FinalTighteningManager.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { ensureMoonflowerStreamLife } from '../atmosphere/MoonflowerStreamLife';
 import { RefreshThrottle } from '../performance/RefreshThrottle';
-import { ensureMoonflowerGladeH110Presentation } from '../world/MoonflowerGladeH110Presentation';
+import { ensureMoonflowerGladeFinalPresentation } from '../world/MoonflowerGladeFinalPresentation';
 
 const FIREFLY_BUTTON_WIDTH = 244;
 const FIREFLY_BUTTON_HEIGHT = 80;
@@ -134,7 +134,7 @@ export class R5FinalTighteningManager {
 
       if (scene.scene.key === 'MoonflowerGladeScene') {
         ensureMoonflowerStreamLife(scene);
-        ensureMoonflowerGladeH110Presentation(scene);
+        ensureMoonflowerGladeFinalPresentation(scene);
       } else if (scene.scene.key === 'FireflyLanternScene') {
         tightenFireflySelector(scene);
       } else if (scene.scene.key === 'CottageInteriorScene') {
