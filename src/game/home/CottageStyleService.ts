67a87ed3a980c26d5c8ebb09ea7c5049fce05515
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-18 15:09:59 +0100

    H2.7 finish style service formatting

--- src/game/home/CottageStyleService.ts
+++ src/game/home/CottageStyleService.ts
@@ -16,9 +16,7 @@ export class CottageStyleService {
 
   public applyStyle(style: HomeStyleState): HomeStyleState {
     if (!isKnownCottageStyle(style)) {
-      throw new Error(
-        'Cottage style contains an unknown wall, wallpaper or floor ID.',
-      );
+      throw new Error('Cottage style contains an unknown wall, wallpaper or floor ID.');
     }
 
     const save = this.saveService.load() ?? this.saveService.createNewGame();
