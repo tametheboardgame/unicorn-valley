68a9f5500ad1316b7f91e42a17c32405d7e99397
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-18 14:10:38 +0100

    H2.6 slim four-wall style cloning

--- src/game/home/CottageStyleService.ts
+++ src/game/home/CottageStyleService.ts
@@ -11,15 +11,7 @@ export class CottageStyleService {
 
   public getPersistedStyle(): HomeStyleState {
     const save = this.saveService.load() ?? this.saveService.createNewGame();
-    return {
-      walls: {
-        back: { ...save.home.style.walls.back },
-        left: { ...save.home.style.walls.left },
-        right: { ...save.home.style.walls.right },
-        front: { ...save.home.style.walls.front },
-      },
-      floorStyleId: save.home.style.floorStyleId,
-    };
+    return structuredClone(save.home.style);
   }
 
   public getResolvedStyle(): HomeStyleState {
@@ -32,15 +24,7 @@ export class CottageStyleService {
     }
 
     const save = this.saveService.load() ?? this.saveService.createNewGame();
-    const nextStyle: HomeStyleState = {
-      walls: {
-        back: { ...style.walls.back },
-        left: { ...style.walls.left },
-        right: { ...style.walls.right },
-        front: { ...style.walls.front },
-      },
-      floorStyleId: style.floorStyleId,
-    };
+    const nextStyle = structuredClone(style);
     this.saveService.save({
       ...save,
       home: {
@@ -52,14 +36,6 @@ export class CottageStyleService {
   }
 
   public resetToDefault(): HomeStyleState {
-    return this.applyStyle({
-      walls: {
-        back: { ...DEFAULT_COTTAGE_STYLE.walls.back },
-        left: { ...DEFAULT_COTTAGE_STYLE.walls.left },
-        right: { ...DEFAULT_COTTAGE_STYLE.walls.right },
-        front: { ...DEFAULT_COTTAGE_STYLE.walls.front },
-      },
-      floorStyleId: DEFAULT_COTTAGE_STYLE.floorStyleId,
-    });
+    return this.applyStyle(structuredClone(DEFAULT_COTTAGE_STYLE));
   }
 }
