79201c6a7ca03777f6c39e5bdc3983584a0d2d54
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-18 14:55:34 +0100

    H2.7 format furniture renderer

--- src/game/home/CottageFurnitureRenderer.ts
+++ src/game/home/CottageFurnitureRenderer.ts
@@ -123,7 +123,8 @@ function renderFireplace(scene: Phaser.Scene, variantId: string): void {
 
   const graphics = scene.add
     .graphics()
-    .setName('cottage-furniture:fireplace').setData('cottage-furniture-variant', variantId)
+    .setName('cottage-furniture:fireplace')
+    .setData('cottage-furniture-variant', variantId)
     .setDepth(furnitureDepth('fireplace'));
   graphics.fillStyle(stone, 1);
   graphics.fillRoundedRect(left - 18, top - 6, fireplace.width + 36, fireplace.height + 25, 18);
@@ -192,7 +193,8 @@ function renderBed(scene: Phaser.Scene, variantId: string): void {
 
   const rear = scene.add
     .graphics()
-    .setName('cottage-furniture:bed-rear').setData('cottage-furniture-variant', variantId)
+    .setName('cottage-furniture:bed-rear')
+    .setData('cottage-furniture-variant', variantId)
     .setDepth(worldDepthForY(bed.y - 42));
 
   rear.fillStyle(timberDark, 1);
@@ -222,7 +224,8 @@ function renderBed(scene: Phaser.Scene, variantId: string): void {
 
   const foreground = scene.add
     .graphics()
-    .setName('cottage-furniture:bed-foreground').setData('cottage-furniture-variant', variantId)
+    .setName('cottage-furniture:bed-foreground')
+    .setData('cottage-furniture-variant', variantId)
     .setDepth(furnitureDepth('bed'));
 
   foreground.fillStyle(blanket, 0.96);
@@ -283,7 +286,8 @@ function renderTeaTable(scene: Phaser.Scene, variantId: string): void {
 
   const tabletop = scene.add
     .graphics()
-    .setName('cottage-furniture:tea-table').setData('cottage-furniture-variant', variantId)
+    .setName('cottage-furniture:tea-table')
+    .setData('cottage-furniture-variant', variantId)
     .setDepth(furnitureDepth('tea-table'));
   tabletop.fillStyle(timberDark, 1);
   tabletop.fillEllipse(table.x, table.y + 13, width + 10, height + 3);
@@ -320,8 +324,10 @@ function renderTeaTable(scene: Phaser.Scene, variantId: string): void {
 }
 
 function renderSofa(scene: Phaser.Scene, variantId: string): void {
-  const [dark, base, seat, pillowLeft, pillowRight, stitch] =
-    getCottageFurniturePalette('sofa', variantId);
+  const [dark, base, seat, pillowLeft, pillowRight, stitch] = getCottageFurniturePalette(
+    'sofa',
+    variantId,
+  );
   const sofa = COTTAGE_INTERIOR_MAP.furnitureLayout.sofa;
   const width = sofa.width * FURNITURE_SCALE.sofa;
   const height = sofa.height * FURNITURE_SCALE.sofa;
@@ -332,7 +338,8 @@ function renderSofa(scene: Phaser.Scene, variantId: string): void {
 
   const graphics = scene.add
     .graphics()
-    .setName('cottage-furniture:sofa').setData('cottage-furniture-variant', variantId)
+    .setName('cottage-furniture:sofa')
+    .setData('cottage-furniture-variant', variantId)
     .setDepth(furnitureDepth('sofa'));
   const armWidth = 36;
   const innerLeft = left + armWidth;
