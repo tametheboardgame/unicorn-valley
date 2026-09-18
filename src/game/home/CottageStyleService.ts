cdd33cc4665eadb4dd1a110b950e557ed70477e7
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-18 07:15:31 +0100

    H2.6 add persisted cottage style service

---
+++ src/game/home/CottageStyleService.ts
@@ -0,0 +1,37 @@
+import type { SaveService } from '../save/SaveService';
+import type { HomeStyleState } from '../save/saveSchema';
+import { DEFAULT_COTTAGE_STYLE, isKnownCottageStyle, resolveCottageStyle } from './CottageStyleCatalogue';
+
+export class CottageStyleService {
+  public constructor(private readonly saveService: SaveService) {}
+
+  public getPersistedStyle(): HomeStyleState {
+    const save = this.saveService.load() ?? this.saveService.createNewGame();
+    return { ...save.home.style };
+  }
+
+  public getResolvedStyle(): HomeStyleState {
+    return resolveCottageStyle(this.getPersistedStyle());
+  }
+
+  public applyStyle(style: HomeStyleState): HomeStyleState {
+    if (!isKnownCottageStyle(style)) {
+      throw new Error('Cottage style contains an unknown wall, wallpaper or floor ID.');
+    }
+
+    const save = this.saveService.load() ?? this.saveService.createNewGame();
+    const nextStyle = { ...style };
+    this.saveService.save({
+      ...save,
+      home: {
+        ...save.home,
+        style: nextStyle,
+      },
+    });
+    return nextStyle;
+  }
+
+  public resetToDefault(): HomeStyleState {
+    return this.applyStyle({ ...DEFAULT_COTTAGE_STYLE });
+  }
+}
