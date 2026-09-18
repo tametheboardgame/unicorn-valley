37ec0081c758c0f084e0acc67cfcf354d7401ca6
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-18 08:15:08 +0100

    H2.6 move room styling to a fixed decorate action

--- src/game/home/CottageDecorateModeState.ts
+++ src/game/home/CottageDecorateModeState.ts
@@ -1,2 +1,3 @@
 export const COTTAGE_DECORATE_MODE_DATA_KEY = 'cottage-decorate-mode';
 export const COTTAGE_DECORATE_TOGGLE_EVENT = 'cottage-decorate-toggle';
+export const COTTAGE_STYLE_OPEN_EVENT = 'cottage-style-open';
