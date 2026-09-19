76992bcf1f0994258a294ab7503df6f947152c55
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-19 13:57:02 +0100

    ui(h2.8): label decoration action as Decorate here

--- src/game/ui/InteractionPrompt.ts
+++ src/game/ui/InteractionPrompt.ts
@@ -28,6 +28,7 @@ const ACTION_PRESENTATION: Record<InteractionActionKind, PrimaryActionPresentati
   buy: { label: 'Buy', icon: 'buy' },
   use: { label: 'Use', icon: 'use' },
   'pick-up': { label: 'Pick up', icon: 'interact' },
+  decorate: { label: 'Decorate here', icon: 'interact' },
   interact: { label: 'Interact', icon: 'interact' },
 };
 
