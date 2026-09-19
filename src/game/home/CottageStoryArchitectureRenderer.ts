d2c6ed0cd0844ccd6af269109f7ff697a629a52a
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-09-19 15:08:43 +0100

    refactor(h2.9): anchor future portal presentation locally

--- src/game/home/CottageStoryArchitectureRenderer.ts
+++ src/game/home/CottageStoryArchitectureRenderer.ts
@@ -11,29 +11,29 @@ import { worldDepthForY } from '../world/WorldDepth';
  */
 export function renderCottageFuturePortalBay(scene: Phaser.Scene): Phaser.GameObjects.Graphics {
   const anchor = resolveCottageSemanticAnchor(COTTAGE_SEMANTIC_ANCHOR_IDS.portalBay);
-  const { x, y } = anchor.position;
   const graphics = scene.add
     .graphics()
+    .setPosition(anchor.position.x, anchor.position.y)
     .setName('cottage-story:future-portal-bay')
-    .setDepth(worldDepthForY(y - 120, -0.4));
+    .setDepth(worldDepthForY(anchor.position.y - 120, -0.4));
 
   graphics.fillStyle(0x765b82, 0.08);
-  graphics.fillEllipse(x, y + 66, 184, 74);
+  graphics.fillEllipse(0, 66, 184, 74);
   graphics.lineStyle(7, 0x856477, 0.72);
-  graphics.strokeRoundedRect(x - 88, y - 104, 176, 174, 70);
+  graphics.strokeRoundedRect(-88, -104, 176, 174, 70);
   graphics.lineStyle(3, 0xe6c879, 0.58);
-  graphics.strokeRoundedRect(x - 74, y - 91, 148, 148, 58);
+  graphics.strokeRoundedRect(-74, -91, 148, 148, 58);
 
   graphics.fillStyle(0xd8c1df, 0.46);
-  graphics.fillCircle(x - 71, y + 54, 7);
-  graphics.fillCircle(x + 71, y + 54, 7);
+  graphics.fillCircle(-71, 54, 7);
+  graphics.fillCircle(71, 54, 7);
   graphics.fillStyle(0xf2d98c, 0.62);
-  graphics.fillCircle(x, y - 92, 5);
+  graphics.fillCircle(0, -92, 5);
 
   graphics.lineStyle(3, 0xc6a6d0, 0.42);
-  graphics.strokeEllipse(x, y + 58, 142, 50);
+  graphics.strokeEllipse(0, 58, 142, 50);
   graphics.lineStyle(2, 0xf1dc9b, 0.45);
-  graphics.strokeEllipse(x, y + 58, 104, 34);
+  graphics.strokeEllipse(0, 58, 104, 34);
 
   return graphics;
 }
